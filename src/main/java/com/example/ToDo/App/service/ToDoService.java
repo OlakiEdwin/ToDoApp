1994818631e381599adc09479fb6ed83d0ed1b2c
OlakiEdwin <olakiedwin@gmail.com>
2023-06-19 12:26:24 +0300

    Made changes

---
+++ src/main/java/com/example/ToDo/App/service/ToDoService.java
@@ -0,0 +1,54 @@
+package com.example.ToDo.App.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.ToDo.App.model.ToDo;
+import com.example.ToDo.App.repo.IToDoRepo;
+
+@Service
+public class ToDoService {
+	
+	@Autowired
+	IToDoRepo Repo;
+	
+	public List<ToDo> getAllToDoItems() {
+		ArrayList<ToDo> todoList = new ArrayList<>();
+		Repo.findAll().forEach(todo -> todoList.add(todo));
+		
+		return todoList;
+	}
+	
+	public ToDo getToDoItemById(Long id) {
+		return Repo.findById(id).get();
+	}
+	
+	public boolean updateStatus(Long id) {
+		ToDo todo = getToDoItemById(id);
+		todo.setStatus("Completed");
+		
+		return saveOrUpdateToDoItem(todo);
+	}
+	
+	public boolean saveOrUpdateToDoItem(ToDo todo) {
+		ToDo updatedObj = Repo.save(todo);
+		
+		if (getToDoItemById(updatedObj.getId()) != null) {
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean deleteToDoItem(Long id) {
+		Repo.deleteById(id);
+		
+		if (getToDoItemById(id) == null) {
+			return true;
+		}
+		return false;
+	}
+
+}
